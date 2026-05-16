c6b889c7b0a72b036e24114fe8b54f230728cbe4
dani <danielmacdand@gmail.com>
2026-05-16 16:03:52 +0800

    test and supabase connection fix

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+// api/supabaseClient.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = "https://irywemujuaeldlkuhfam.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyeXdlbXVqdWFlbGRsa3VoZmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MTQ0MjMsImV4cCI6MjA5NDQ5MDQyM30.SB6JHFWb6GzXfedPzXV5fueTOBh-k_TiaSUcmaP338o";
